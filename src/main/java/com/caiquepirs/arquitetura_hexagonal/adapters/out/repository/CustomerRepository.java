022f4a7fd7480b8f65202ce127ba852f731f2d3e
Caique Pires <c.pires@ba.estudante.senai.br>
2025-09-11 21:57:18 +0100

    feat: Implements the mongoDB repository

---
+++ src/main/java/com/caiquepirs/arquitetura_hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.caiquepirs.arquitetura_hexagonal.adapters.out.repository;
+
+import com.caiquepirs.arquitetura_hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
