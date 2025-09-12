b66f107b929bbd062d2b53acf3cb1222cf32a1a0
Caique Pires <c.pires@ba.estudante.senai.br>
2025-09-12 16:29:34 +0100

    feat: Implement updateCustomer useCase and output port

---
+++ src/main/java/com/caiquepirs/arquitetura_hexagonal/application/core/usecases/UpdateCustomerUseCase.java
@@ -0,0 +1,31 @@
+package com.caiquepirs.arquitetura_hexagonal.application.core.usecases;
+
+import com.caiquepirs.arquitetura_hexagonal.application.core.domain.Customer;
+import com.caiquepirs.arquitetura_hexagonal.application.ports.FindCustomerByIdInputPort;
+import com.caiquepirs.arquitetura_hexagonal.application.ports.out.FindAddressByZipCodeOutPort;
+import com.caiquepirs.arquitetura_hexagonal.application.ports.out.UpdateCustomerOutputPort;
+
+public class UpdateCustomerUseCase {
+
+    private final UpdateCustomerOutputPort updateCustomerOutputPort;
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final FindAddressByZipCodeOutPort findAddressByZipCodeOutPort;
+
+    public UpdateCustomerUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                 FindAddressByZipCodeOutPort findAddressByZipCodeOutPort,
+                                 UpdateCustomerOutputPort updateCustomerOutputPort){
+
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.findAddressByZipCodeOutPort = findAddressByZipCodeOutPort;
+        this.updateCustomerOutputPort = updateCustomerOutputPort;
+    }
+
+    public void update(Customer customer, String zipCode){
+        findCustomerByIdInputPort.find(customer.getId());
+        findAddressByZipCodeOutPort.find(zipCode);
+        updateCustomerOutputPort.update(customer);
+    }
+
+
+
+}
---
+++ src/main/java/com/caiquepirs/arquitetura_hexagonal/application/ports/out/UpdateCustomerOutputPort.java
@@ -0,0 +1,8 @@
+package com.caiquepirs.arquitetura_hexagonal.application.ports.out;
+
+import com.caiquepirs.arquitetura_hexagonal.application.core.domain.Customer;
+
+public interface UpdateCustomerOutputPort {
+
+    Customer update(Customer customer);
+}
