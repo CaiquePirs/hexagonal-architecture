182109d1a2bc0dd995c630053be9e0196f898b80
Caique Pires <c.pires@ba.estudante.senai.br>
2025-09-13 19:17:28 +0100

    feat: implements Kafka configurations

--- src/main/java/com/caiquepirs/arquitetura_hexagonal/adapters/in/controllers/request/CustomerMapper.java
+++ src/main/java/com/caiquepirs/arquitetura_hexagonal/adapters/in/controllers/request/CustomerMapper.java
@@ -3,14 +3,10 @@ package com.caiquepirs.arquitetura_hexagonal.adapters.in.controllers.request;
 import com.caiquepirs.arquitetura_hexagonal.adapters.in.controllers.response.CustomerResponse;
 import com.caiquepirs.arquitetura_hexagonal.application.core.domain.Customer;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "address", ignore = true)
-    @Mapping(target = "isValidCpf", ignore = true)
     Customer toEntity(CustomerRequest customerRequest);
 
     CustomerResponse toResponse(Customer customer);
